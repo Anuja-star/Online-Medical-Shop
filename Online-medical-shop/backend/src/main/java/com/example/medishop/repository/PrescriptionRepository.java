6ba8ea5933be9e720f0303d51713b66fc1f5fa3a
Anuja-star <anujachavan8641@gmail.com>
2026-01-26 21:12:39 +0530

    Remove duplicate project folder and zip

--- Online-medical-shop/backend/src/main/java/com/example/medishop/repository/PrescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.medishop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.medishop.model.Prescription;
-
-public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-}
-
